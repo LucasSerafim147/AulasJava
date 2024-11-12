fc7b71e47a91144cf99cf7b13c207913046c4799
Lucas Henrique <lucashenrique74148@gmail.com>
2024-11-12 16:22:44 -0300

    alo

--- Aula01/src/Aula01/exemplo8.java
+++ Aula01/src/Aula01/exemplo8.java
@@ -4,9 +4,10 @@ public class exemplo8 {
 	public static void main(String[] args) {
 	int idade = 15;
 	boolean amigoDoDono = true;
-	
-			if (idade<18 && amigoDoDono ==false) {System.out.println("Não pode Entrar");
-			}else {System.out.println("Pode entrar");}
+	if (idade<18 && amigoDoDono ==false) {System.out.println("Não pode Entrar");}
+		else
+			{System.out.println("Pode entrar");
+		}
 	}
 
 }
