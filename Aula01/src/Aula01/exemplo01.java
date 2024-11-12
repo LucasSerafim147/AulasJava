17eb38fd2c7d5c67193768f7aef78ae414e8f9eb
Lucas Henrique <lucashenrique74148@gmail.com>
2024-11-12 14:54:47 -0300

    exemplo 01

---
+++ Aula01/src/Aula01/exemplo01.java
@@ -0,0 +1,15 @@
+package Aula01;
+
+public class exemplo01 {
+	
+	public static void main(String[] args) {
+		int x = 10;
+		System.out.println(x++); 
+		System.out.println(++x);
+		System.out.println(x--);
+		System.out.println(--x);
+		
+		
+		
+	}
+}
