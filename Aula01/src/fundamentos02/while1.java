33d14cf9a53cf633a01f85a3c85410187ffee0ac
Lucas Henrique <lucashenrique74148@gmail.com>
2024-11-21 14:10:37 -0300

    while

---
+++ Aula01/src/fundamentos02/while1.java
@@ -0,0 +1,30 @@
+package fundamentos02;
+import java.util.Scanner;
+public class while1 {
+public static void main(String[] args) {
+	int alunos;
+	float media;
+	float notas = 0 ;
+	int cont = 1;
+	Scanner entrada = new Scanner(System.in);
+	System.out.println("Insira a quantidade de alunos na sala de aula: ");
+	alunos = entrada.nextInt();
+	
+	
+	
+	while(cont <= alunos) {
+	
+	System.out.println("Insira a nota "+ cont+ "° do aluno: ");
+	notas = notas + entrada.nextFloat();
+	cont += 1;
+	}
+	
+	
+	media = notas/alunos;
+	System.out.println(media);
+	
+	
+	
+	
+}
+}
