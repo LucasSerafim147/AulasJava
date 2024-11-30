031cc8a3abd4379b0ba1487eadc0a08f7442d900
Lucas Henrique <lucashenrique74148@gmail.com>
2024-11-30 14:19:36 -0300

    POO

--- aulasPOOf/src/JavaPOO/Cidade.java
+++ aulasPOOf/src/JavaPOO/Cidade.java
@@ -3,10 +3,11 @@ package JavaPOO;
 public class Cidade {
 	public static void main(String[] args) {
 		Pessoa p1 = new Pessoa();
-		p1.nome = "Lucas";
-		p1.peso = 68.00;
-		p1.idade = 19;
+		Pessoa p2 = new Pessoa();
+		p1.cadastrar();
+	//	p2.cadastrar();
 		System.out.printf("%s que tem %d anos e pesa %.2f\n",p1.nome, p1.idade, p1.peso);
 		p1.comer();
+		
 }
 }
--- aulasPOOf/src/JavaPOO/Pessoa.java
+++ aulasPOOf/src/JavaPOO/Pessoa.java
@@ -1,10 +1,28 @@
 package JavaPOO;
 
+import java.util.Scanner;
+
 public class Pessoa {
 	String nome;
 	Double peso;
 	int idade;
+	
 	public void comer() {
-		System.out.println(this.nome + " Foi comer...");
+		System.out.println(this.nome+ " foi comer");
+	}
+	
+	public void cadastrar() {
+		Scanner scr = new Scanner(System.in);
+		System.out.println("Cadastre seu nome: ");
+		this.nome = scr.nextLine();
+		System.out.println("Insira sua idade: ");
+		this.idade = scr.nextInt();
+		System.out.println("Insira seu peso: ");
+		this.peso = scr.nextDouble();
+		scr.close();
+		
+		
+		
 	}
 }
+
